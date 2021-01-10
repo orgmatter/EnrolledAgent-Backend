a1a03ec22bb276565fa0500cf938d6c93a3cb3ae
Joseph <joseph.abayomi@pbgdigital.co.uk>
2021-01-10 09:31:42 +0100

    Merge conflict

--- src/redux/axios/index.js
+++ src/redux/axios/index.js
@@ -2,4 +2,8 @@ import axios from "axios";
 
 export default axios.create({
   baseURL: "https://api.enrolledagent.org",
+  'Content-Type': 'application/json',
+  'apikey': 'fsdjkahdgjknsdfhvbjknsdjfbglksvajkbhdkgncvb',
+  "Authorization": `Bearer ${localStorage.getItem("token")}`,
+  "Access-Control-Allow-Origin": "*"
 });
