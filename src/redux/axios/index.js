69e56c201f3e15c83ef77cfc81e25a5dd43d511c
Joseph <joseph.abayomi@pbgdigital.co.uk>
2021-01-10 18:10:51 +0100

    Fixed merge conflicts

--- src/redux/axios/index.js
+++ src/redux/axios/index.js
@@ -2,12 +2,8 @@ import axios from "axios";
 
 export default axios.create({
   baseURL: "https://api.enrolledagent.org",
-  headers: {
-    'Content-Type': 'application/json',
-    "Access-Control-Allow-Origin": "*",
-    "Access-Control-Allow-Methods": "GET,PUT,POST,DELETE,PATCH,OPTIONS",
-    'apikey': 'fsdjkahdgjknsdfhvbjknsdjfbglksvajkbhdkgncvb',
-    "Authorization": `Bearer ${localStorage.getItem("token")}`
-}
-
+  'Content-Type': 'application/json',
+  'apikey': 'fsdjkahdgjknsdfhvbjknsdjfbglksvajkbhdkgncvb',
+  "Authorization": `Bearer ${localStorage.getItem("token")}`,
+  "Access-Control-Allow-Origin": "*"
 });
